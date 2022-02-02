7f7e11740f93542eaf3fd1c9b37ffe164fc32316
Matteo Pernarella <matteo.pernarella@gmail.com>
2022-02-02 12:10:43 +0100

    re-formatted

--- pwdge0n.sh
+++ pwdge0n.sh
@@ -20,7 +20,7 @@
 trap '[ "$?" -ne 0 ] && printf "Error: An error occurred\n"' EXIT
 
 show_help() {
-    cat <<EOF
+	cat <<EOF
 
 Usage:
 
@@ -33,19 +33,17 @@ Options:
 
 PASSWORD LENGTH MUST BE >= 8
 EOF
-    exit 0
+	exit 0
 }
 
-if [ $# -gt 3 ];
-then
-    echo "Too many arguments.\n"
-    show_help
-    exit 1
-elif [ $# -lt 1 ];
-then
-    echo "You must specify at least one argument (the pasword length for example...).\n"
-    show_help
-    exit 1
+if [ $# -gt 3 ]; then
+	echo "Too many arguments.\n"
+	show_help
+	exit 1
+elif [ $# -lt 1 ]; then
+	echo "You must specify at least one argument (the pasword length for example...).\n"
+	show_help
+	exit 1
 fi
 
 # Character used to build the password
@@ -59,96 +57,96 @@ E=0
 
 # Arguments parsing
 while :; do
-    case $1 in
-    -h)
-        show_help
-        ;;
-    -e)
-        E=1
-        shift
-        ;;
-    -a)
-        MAX=3
-        shift
-        ;;
-    *)
-        if [ -z $LENGTH ] && [ -n $1 ]; then
-            if ! [ -z $(grep "[0-9]" <<<$1) ]; then
-                if [ $1 -gt 7 ]; then
-                    LENGTH=$1
-                    shift
-                else
-                    show_help
-                fi
-            else
-                show_help
-            fi
-        else
-            break
-        fi
-        ;;
-    esac
+	case $1 in
+	-h)
+		show_help
+		;;
+	-e)
+		E=1
+		shift
+		;;
+	-a)
+		MAX=3
+		shift
+		;;
+	*)
+		if [ -z $LENGTH ] && [ -n $1 ]; then
+			if ! [ -z $(grep "[0-9]" <<<$1) ]; then
+				if [ $1 -gt 7 ]; then
+					LENGTH=$1
+					shift
+				else
+					show_help
+				fi
+			else
+				show_help
+			fi
+		else
+			break
+		fi
+		;;
+	esac
 done
 
 # Extract character from one of the array
 extract() {
-    INDEX=0
-    RAND=$(( RANDOM % ${#1}/2 ))
-    for i in $1; do
-        if [ $INDEX -eq $RAND ]; then
-            printf "%s" "$i"
-        fi
-        INDEX=$((INDEX + 1))
-    done
+	INDEX=0
+	RAND=$((RANDOM % ${#1} / 2))
+	for i in $1; do
+		if [ $INDEX -eq $RAND ]; then
+			printf "%s" "$i"
+		fi
+		INDEX=$((INDEX + 1))
+	done
 }
 
 # Check that passowrd contains at least one char of every group
-checkpass(){
-    for i in [a-z] [A-Z] [0-9]; do
-        GOOD=$(grep "$i" <<<$A;)
-        if [ -z $GOOD ]; then
-            GOOD=0
-            return 1
-        fi
-    done
-    if [ $MAX -eq 4 ]; then
-        GOOD=$(grep "[[:punct:]]" <<<$1)
-        if [ -z $GOOD ]; then
-            return 1
-        fi
-    fi
-    return 0
+checkpass() {
+	for i in [a-z] [A-Z] [0-9]; do
+		GOOD=$(grep "$i" <<<$A)
+		if [ -z $GOOD ]; then
+			GOOD=0
+			return 1
+		fi
+	done
+	if [ $MAX -eq 4 ]; then
+		GOOD=$(grep "[[:punct:]]" <<<$1)
+		if [ -z $GOOD ]; then
+			return 1
+		fi
+	fi
+	return 0
 }
 
 # Main function used to generate password
 genpass() {
-    for i in $(seq 1 $LENGTH); do
-        case $(( RANDOM % MAX )) in
-        0)
-            CHAR=$(extract "$LOWER")
-            ;;
-        1)
-            CHAR=$(extract "$UPPER")
-            ;;
-        2)
-            CHAR=$(extract "$NUMBERS")
-            ;;
-        3)
-            if [ $E -gt 0 ]; then
-                CHAR=$(extract "$RSYMBOLS")
-            else
-                CHAR=$(extract "$SYMBOLS")
-            fi
-            ;;
-        esac
-        PASSWORD="$PASSWORD$CHAR"
-    done
-    checkpass "$PASSWORD"
-    if ! [ -z $? ]; then
-        printf "%s\n" "$PASSWORD"
-    else
-        genpass
-    fi
+	for i in $(seq 1 $LENGTH); do
+		case $((RANDOM % MAX)) in
+		0)
+			CHAR=$(extract "$LOWER")
+			;;
+		1)
+			CHAR=$(extract "$UPPER")
+			;;
+		2)
+			CHAR=$(extract "$NUMBERS")
+			;;
+		3)
+			if [ $E -gt 0 ]; then
+				CHAR=$(extract "$RSYMBOLS")
+			else
+				CHAR=$(extract "$SYMBOLS")
+			fi
+			;;
+		esac
+		PASSWORD="$PASSWORD$CHAR"
+	done
+	checkpass "$PASSWORD"
+	if ! [ -z $? ]; then
+		printf "%s\n" "$PASSWORD"
+	else
+		genpass
+	fi
 }
 
 genpass
